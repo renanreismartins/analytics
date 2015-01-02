4750737b14e46ab5a9d46fbcfea8e31c8d0dcdf9
Renan Reis <renanreismartins@gmail.com>
2015-01-02 15:18:22 -0300

    Changed common-user-ratings to return two separated lists, to use the lists with pearson-correlation. Nearest neighbors was changed to use the lists and sort by > because pearson-correlation. Avoid division by 0 in pearson correlation.

--- src/chapter2/core.clj
+++ src/chapter2/core.clj
@@ -9,34 +9,34 @@
 (def data
   {
    :Angelica {"Blues Traveler" 3.5
-              "Broken Bells" 2.0
+              "Broken Bells" 2
               "Norah Jones" 4.5
-              "Phoenix" 5.0
+              "Phoenix" 5
               "Slightly Stoopid" 1.5
               "The Strokes" 2.5,
-              "Vampire Weekend" 2.0}
+              "Vampire Weekend" 2}
 
-   :Bill {"Blues Traveler" 2.0
+   :Bill {"Blues Traveler" 2
           "Broken Bells" 3.5
-          "Deadmau5" 4.0
-          "Phoenix" 2.0
+          "Deadmau5" 4
+          "Phoenix" 2
           "Slightly Stoopid" 3.5
-          "Vampire Weekend" 3.0}
+          "Vampire Weekend" 3}
 
-   :Chan: {"Blues Traveler" 5.0
-           "Broken Bells" 1.0
-           "Deadmau5" 1.0
-           "Norah Jones" 3.0
-           "Phoenix" 5
-           "Slightly Stoopid" 1.0}
+   :Chan {"Blues Traveler" 5
+          "Broken Bells" 1
+          "Deadmau5" 1
+          "Norah Jones" 3
+          "Phoenix" 5
+          "Slightly Stoopid" 1}
 
-   :Dan {"Blues Traveler" 3.0
-         "Broken Bells" 4.0
+   :Dan {"Blues Traveler" 3
+         "Broken Bells" 4
          "Deadmau5" 4.5
-         "Phoenix" 3.0
+         "Phoenix" 3
          "Slightly Stoopid" 4.5
-         "The Strokes" 4.0
-         "Vampire Weekend" 2.0}
+         "The Strokes" 4
+         "Vampire Weekend" 2}
 
    :Hailey {"Broken Bells" 4
             "Deadmau5" 1
@@ -44,12 +44,12 @@
             "The Strokes" 4
             "Vampire Weekend" 1}
 
-   :Sam {"Blues Traveler" 5.0
-         "Broken Bells" 2.0
-         "Norah Jones" 3.0
-         "Phoenix" 5.0
-         "Slightly Stoopid" 4.0
-         "The Strokes" 5.0}
+   :Sam {"Blues Traveler" 5
+         "Broken Bells" 2
+         "Norah Jones" 3
+         "Phoenix" 5
+         "Slightly Stoopid" 4
+         "The Strokes" 5}
 
    :Veronica {"Blues Traveler" 3
               "Norah Jones" 5
@@ -63,17 +63,27 @@
             "Phoenix" 5
             "Slightly Stoopid" 4.5
             "The Strokes" 4
-            "Vampire Weekend" 4.0}
+            "Vampire Weekend" 4}
    }
   )
 
 
+(comment
+  (defn common-users-ratings [user-1 user-2]
+    (let [user-1-data (user-1 data)
+          user-2-data (user-2 data)
+          bands-in-common (keys (select-keys user-1-data (keys user-2-data)))
+          users-ratings (fn [band] [(user-1-data band) (user-2-data band)])]
+      (map users-ratings bands-in-common)))) ; for manhattan-distance
+
+(def select-values (comp vals select-keys))
+
 (defn common-users-ratings [user-1 user-2]
   (let [user-1-data (user-1 data)
         user-2-data (user-2 data)
-        bands-in-common (keys (select-keys user-1-data (keys user-2-data)))
-        users-ratings (fn [band] [(user-1-data band) (user-2-data band)])]
-    (map users-ratings bands-in-common)))
+        bands-in-common (keys (select-keys user-1-data (keys user-2-data)))]
+    [(select-values user-1-data bands-in-common) (select-values user-2-data bands-in-common)]))
+
 
 (defn manhattan-distance [ratings]
   (->> ratings
@@ -83,22 +93,11 @@
 (defn nearest-neighbor [user]
   (let [other-users (remove #{user} (keys data))]
     (->> other-users
-         (map (fn [neighbor] [neighbor (manhattan-distance (common-users-ratings user neighbor))]))
-         (sort-by last))))
-
-(defn recommend [user]
-  (let [nearest (ffirst(nearest-neighbor user))
-        user-bands (set (keys (user data)))
-        nearest-bands (keys (nearest data))
-        not-in-common-bands (remove user-bands nearest-bands)]
-
-    (->> not-in-common-bands
-         (map (fn [band] [band ((nearest data) band)]))
+         (map (fn [neighbor]
+                [neighbor (apply pearson-correlation (common-users-ratings user neighbor))]))
+         ;[neighbor (manhattan-distance (common-users-ratings user neighbor))])) ;for manhattan-distance
          (sort-by last >))))
-
-
-(recommend :Hailey)
-
+;(sort-by last)))) for manhattan-distance
 
 (defn exponential-sum-divided [ratings]
   (/ (expt (reduce + ratings) 2)
@@ -126,16 +125,15 @@
         numerator-second-expression (/ (* ratings-1-sum ratings-2-sum) (count ratings-1))]
     (- numerator-first-expression numerator-second-expression)))
 
-(defn pearson-correlation [ratings-1 ratings-2]
-  (/ (calc-numerator ratings-1 ratings-2)
-     (calc-denominator ratings-1 ratings-2)))
+(defn pearson-correlation [ratings-1 ratings-2] ;use let here
+  (if (zero? (calc-denominator ratings-1 ratings-2))
+    0
+    (/ (calc-numerator ratings-1 ratings-2)
+       (calc-denominator ratings-1 ratings-2))))
 
 
-(pearson-correlation (list 4.75 4.5 5 4.25 4) (list 4 3 5 2 1))
-
-
-(defn k-nearest-neighbor [user k]
-  (let [k-nearest-neighbors (take k (nearest-neighbor user));mudar nearest para mapa?
+(defn recommend [user k]
+  (let [k-nearest-neighbors (take k (nearest-neighbor user)) ;change nearest to a map?
         neighbors-ratings (map #(second %) k-nearest-neighbors)
         distance (reduce + 0 neighbors-ratings)
 
@@ -147,19 +145,14 @@
                                                        neighbor-data (neighbor data)]
                                                    {neighbor (apply dissoc neighbor-data user-bands)}))
                                                neighbor-and-weight)
-
         neighbors-and-not-in-common-bands-map (reduce merge {} neighbors-and-not-in-common-bands)
 
         recomendations (map (fn [[neighbor weight]]
-                  (fmap (fn [rate]
-                          (* rate weight))
-                        (neighbor neighbors-and-not-in-common-bands-map)))
-                neighbor-and-weight)]
+                              (fmap (fn [rate]
+                                      (* rate weight))
+                                    (neighbor neighbors-and-not-in-common-bands-map)))
+                            neighbor-and-weight)]
 
     (sort-by last > (apply merge-with + recomendations))))
 
-
-
-
-(k-nearest-neighbor :Hailey 5)
-
+(recommend :Hailey 1)
