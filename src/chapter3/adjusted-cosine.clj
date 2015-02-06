153505749f6f2d706534d24974ac6a3217604ae2
Renan Reis <renanreismartins@gmail.com>
2015-02-06 18:45:47 -0200

    calc-normalized-ratings receives the ratings and band names to reuse it in the denominator. Cosine similarity finished :D

--- src/chapter3/adjusted-cosine.clj
+++ src/chapter3/adjusted-cosine.clj
@@ -1,4 +1,5 @@
 (use 'clojure.set)
+(use 'clojure.math.numeric-tower)
 
 (def data
   {
@@ -41,24 +42,30 @@
     {user {:average (/ (apply + ratings) (count ratings))}}))
 
 (defn user-ratings-for-bands [bands [user band-ratings]]
-  {user {:ratings (select-values band-ratings bands)}})
+  {user {:ratings (select-keys band-ratings bands)}})
 
 (defn calc-normalized-ratings [ratings-and-average]
   (map (fn [rating]
-         (- rating (:average ratings-and-average)))
-       (:ratings ratings-and-average)))
+         (- rating (:average ratings-and-average))) (vals (:ratings ratings-and-average))))
 
 (defn multiply-and-sum [normalized-ratings]
-  (reduce + (map (fn [[rating1 rating2]] (* rating1 rating2)) normalized-ratings)))
+  (reduce + (map (partial apply *)
+                 #_(fn [[rating1 rating2]] (* rating1 rating2)) normalized-ratings)))
 
-(defn numerator [bands]
+(defn to-ratings-per-band-and-user [acc [r1 r2]]
+  [(+ (expt r1 2) (first acc)) (+ (expt r2 2) (second acc))])
+
+(defn cosine-similarity [bands]
   (let [v-user-ratings-with-common-bands (user-ratings-with-common-bands bands)
         users-and-average-rating (reduce conj {} (map to-user-and-average-rating v-user-ratings-with-common-bands))
         users-and-ratings (reduce conj {} (map (partial user-ratings-for-bands bands) v-user-ratings-with-common-bands))
         users-and-ratings-and-average (merge-with union users-and-average-rating users-and-ratings)
-        normalized-ratings (map calc-normalized-ratings (vals users-and-ratings-and-average))]
-    (multiply-and-sum normalized-ratings)))
+        normalized-ratings (map calc-normalized-ratings (vals users-and-ratings-and-average))
+        cosine-numerator (multiply-and-sum normalized-ratings)
+        ratings-sum-per-user-and-band (reduce to-ratings-per-band-and-user [0 0] normalized-ratings)
+        cosine-denominator (* (sqrt (first ratings-sum-per-user-and-band))
+                              (sqrt (second ratings-sum-per-user-and-band)))]
+    (/ cosine-numerator cosine-denominator )))
 
 
-(numerator ["Kacey Musgraves" "Imagine Dragons"])
-
+(cosine-similarity ["Kacey Musgraves" "Imagine Dragons"])
